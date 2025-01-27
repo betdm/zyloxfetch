091a1de85ffaedd2e7201ef0fd7a740a2f5b2c1f
betdm <whysoinstagood@gmail.com>
2025-01-27 12:55:38 +0100

    Update zyloxfetch.bash

--- zyloxfetch.bash
+++ zyloxfetch.bash
@@ -1,122 +1,20 @@
 #!/bin/bash
 
 # Function to display ASCII art based on OS
-get_ascii_art() {
-    case "$OS" in
-        *Ubuntu*)
-            cat << "EOF"
-             .-/+oossssoo+/-.              
-         `:+ssssssssssssssssss+:`          
-       -+ssssssssssssssssssyyssss+-        
-     .ossssssssssssssssssdMMMNysssso.    
-    /ssssssssssshdmmNNmmyNMMMMhssssss/  
-   +ssssssssshmydMMMMMMMNddddyssssssss+ 
-   /sssssssshNMMMyhhyyyyhmNMMMNhssssss/ 
-  .ssssssssdMMMNhsssssssssshNMMMdssssss. 
- +sssshhhyNMMNyssssssssssssyNMMMysssssss+ 
- ossyNMMMNyMMhsssssssssssssshmmmhssssssso 
- ossyNMMMNyMMhsssssssssssssshmmmhssssssso 
- +sssshhhyNMMNyssssssssssssyNMMMysssssss+ 
-  .ssssssssdMMMNhsssssssssshNMMMdssssss. 
-   /sssssssshNMMMyhhyyyyhdNMMMNhssssss/ 
-   +ssssssssshmydMMMMMMMNddddyssssssss+ 
-    /ssssssssssshdmNNNNmyNMMMMhssssss/  
-     .ossssssssssssssssssdMMMNysssso.    
-       -+sssssssssssssssssyyyssss+-        
-        `:+ssssssssssssssssss+:`          
-            .-/+oossssoo+/-.              
-EOF
-            ;;
-        *Debian*)
-            cat << "EOF"
-       _,met$$$$$gg.           
-    ,g$$$$$$$$$$$$$$$P.        
-  ,g$$P"     """Y$$.".         
- ,$$P'              `$$$.      
-',$$P       ,ggs.     `$$b:    
-`d$$'     ,$P"'   .    $$$     
- $$P      d$'     ,    $$P     
- $$:      $$.   -    ,d$$'     
- $$;      Y$b._   _,d$P'       
- Y$$.    `.`"Y$$$$P"'          
- `$$b      "-.__               
-  `Y$$                         
-   `Y$$.                       
-     `$$b.                     
-       `Y$$b.                  
-          `"Y$b._              
-              `""""            
-EOF
-            ;;
-        *Arch*)
-            cat << "EOF"
-                 -`                 
-                .o+`                
-               `ooo/                
-              `+oooo:               
-             `+oooooo:              
-             -+oooooo+:             
-           `/:-:++oooo+:            
-          `/++++/+++++++:           
-         `/++++++++++++++:          
-        `/+++ooooooooooooo/`        
-       ./ooosssso++osssssso+`       
-      .oossssso-````/ossssss+`      
-     -osssssso.      :ssssssso.     
-    :osssssss/        osssso+++`    
-   /ossssssss/        +ssssooo/-    
- /ossssso+/:-        -:/+osssso+-   
-`..--                  `-/+osssso+  
-                                  ` 
-EOF
-            ;;
-        *Fedora*)
-            cat << "EOF"
-          /:-------------:/          
-       :-------------------::       
-     :-----------/shhOHbmp---:\     
-   /-----------omMMMNNNMMD  ---\    
-  :-----------sMMMMNMNMP.    ---:   
- :-----------:MMMdP-------    ---:  
-,------------:MMMd--------    ---:  
-:------------:MMMd-------    .---:  
-:----    oNMMMMMMMMMNho     .----:  
-:--     .+shhhMMMmhhy++   .------/  
-:-    -------:MMMd--------------:   
-:-   --------/MMMd-------------;    
-:-    ------/hMMMy------------:     
-:-- :dMNdhhdNMMNo------------;      
-:---:sdNMMMMNds:------------:       
-:------:://:-------------::         
-:---------------------://           
-                                  
-EOF
-            ;;
-        *CentOS*)
-            cat << "EOF"
-                 _               
-              _/X               
-              /X/               
-             /X/               
-            /XX/       _       
-          _/XXX/     _/X/_     
-        _/XXXXXX/_ _/XX/X/     
-       /XXXXXXXXXX/XXXX/X/     
-      /XXXXX/XXXXX/XXXX/X/     
-     /XXXXX/XXXXX/XXXXXX/X/    
-    /XXXXX/XXXXX/XXXXXX/X/     
-   /XXXXX/XXXXX/XXXXXX/X/      
-  /XXXXX/XXXXX/XXXXXX/X/       
- /XXXXX/XXXXX/XXXXXX/X/        
-         /XXXXX/XXXXX/X/         
-        /XXXXX/XXXXX/X/          
-        /XXXXX/XXXXX/X/           
-EOF
-            ;;
-        *)
-            echo "ASCII art not available for OS: $OS"
-            ;;
-    esac
+get_ascii_art() { ```
+         ,. –  - .,  °           ,-·-.          ,'´¨;             ,.  '              , ·. ,.-·~·.,   ‘                           .,'                                ,.-·~ ^~, ‘                 _,.,  °             ,  . .,  °                 ,. - .,               .·¨'`;        ,.·´¨;\   
+        ';_,.., _     '`. '       ';   ';\      ,'´  ,':\'         /   ';\             /  ·'´,.-·-.,   `,'‚         ,.,           ,'´  ;\                             .'´  ,.-·~-.,':\          ,.·'´  ,. ,  `;\ '     ;'´    ,   ., _';\'          ,·'´ ,. - ,   ';\          ';   ;'\       ';   ;::\  
+         \:::::::::::';   ,'\       ;   ';:\   .'   ,'´::'\'      ,'   ,'::'\           /  .'´\:::::::'\   '\ °       \`, '`·.    ,·' ,·´\::'\                          ;   ,':\::::::'\::\       .´   ;´:::::\`'´ \'\     \:´¨¯:;'   `;::'\:'\     ,·´  .'´\:::::;'   ;:'\ '       ;   ;::'\      ,'   ;::'; 
+          '\_;::;:,·´  .·´::\‘     '\   ';::;'´  ,'´::::;'      ,'    ;:::';'       ,·'  ,'::::\:;:-·-:';  ';\‚        \:';  '`·,'´,·´::::'\:;'                         ;'   ;:::'_-·~-\;' ‘     /   ,'::\::::::\:::\:'      \::::;   ,'::_'\;'    /  ,'´::::'\;:-/   ,' ::;  '     ;  ;::_';,. ,.'   ;:::';°
+              , '´ .·´:::::;'         \  '·:'  ,'´:::::;' '      ';   ,':::;'       ;.   ';:::;´       ,'  ,':'\‚        `';'\    ,':::::;·´                          ,.'    'ª*'´ __'`;\ °     ;   ;:;:-·'~^ª*';\'´            ,'  ,'::;'  ‘    ,'   ;':::::;'´ ';   /\::;' '     .'     ,. -·~-·,   ;:::'; '
+            .´  .'::::::;·´'           '·,   ,'::::::;'´        ;  ,':::;' '        ';   ;::;       ,'´ .'´\::';‚         ,·´,   \:;·´    '                         `',   ;\¯::::::::\:\‘     ;  ,.-·:*'´¨'`*´\::\ '           ;  ;:::;  °    ;   ;:::::;   '\*'´\::\'  °     ';   ;'\::::::::;  '/::::;  
+        .·´ ,·´:::::;·´                ,'  /::::::;'  '       ,'  ,'::;'           ';   ':;:   ,.·´,.·´::::\;'°     .·´ ,·´:\   '\                                 ;   ;::\;::_:_::\;'    ;   ;\::::::::::::'\;'            ;  ;::;'  ‘     ';   ';::::';    '\::'\/.'         ;  ';:;\;::-··;  ;::::;   
+     ,·´  .´;::–·~^*'´';\‚          ,´  ';\::::;'  '         ;  ';_:,.-·´';\‘     \·,   `*´,.·'´::::::;·´     ,·´  .;:::::'\   ';    '                           ;  ';:::;        '       ;  ;'_\_:;:: -·^*';\            ;  ;::;'‚        \    '·:;:'_ ,. -·'´.·´\‘       ':,.·´\;'    ;' ,' :::/  '  
+     '.,_ ,. -·~:*'´¨¯:\:\ °       \`*ª'´\\::/‘            ',   _,.-·'´:\:\‘     \\:¯::\:::::::;:·´       ;    '.·'\::::;'   ,'\'                              ';  ;::';                ';    ,  ,. -·:*'´:\:'\°          ',.'\::;'‚         '\:` ·  .,.  -·:´::::::\'       \:::::\    \·.'::::;     
+      \:::::::::::::::::::\;          '\:::::\';  '            \¨:::::::::::\';      `\:::::\;::·'´  °        ;·-'´:::::\·´ \·:´:::\                              \*´\:';        '‚        \`*´ ¯\:::::::::::\;' '          \::\:;'‚           \:::::::\:::::::;:·'´'          \;:·´     \:\::';      
+       \:;_;::-·~^*'´¨¯'              `*ª'´‘                 '\;::_;:-·'´‘            ¯                    \::::;:·'     '\;:·'´                                '\::\;     '              \:::::\;::-·^*'´                \;:'      ‘         `· :;::\;::-·´                          `·\;'       
+                                          '                      '¨                      ‘                     `*'´           ‘                                     `*´‘                     `*´¯                           °                                                           '        
+```
 }
 
 # Function to get system information
